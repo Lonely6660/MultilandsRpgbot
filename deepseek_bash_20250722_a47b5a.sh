6880384a365e85eddf2281e19456d78595a879b6
Lonely6660 <130300425+Lonely6660@users.noreply.github.com>
2025-07-22 16:27:43 -0300

    Update deepseek_bash_20250722_a47b5a.sh

--- deepseek_bash_20250722_a47b5a.sh
+++ deepseek_bash_20250722_a47b5a.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/yourusername/your-repo.git
-cd your-repo
+git clone https://github.com/Lonely6660/test.rpg727672516/
+cd  https://github.com/Lonely6660/test.rpg727672516/
